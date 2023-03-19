59aac63c112a2efe95480e8b8705a056a2337aa5
Graydon von Gossler <graydonvg@gmail.com>
2023-03-19 17:01:16 +0200

    changed vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), eslint()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 });
