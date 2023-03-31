460118d4c35852307c891f251bc707a08c7e715f
GraydonvG <graydonvg@gmail.com>
2023-03-31 14:44:29 +0200

    fixing syntax error

--- src/routes/shop/shop.route.jsx
+++ src/routes/shop/shop.route.jsx
@@ -10,7 +10,7 @@ function Shop() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchCategoriesAsync);
+    dispatch(fetchCategoriesAsync());
   }, [dispatch]);
 
   return (
