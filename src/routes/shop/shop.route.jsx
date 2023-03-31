e55601a488f9f4029d89d48c2a5e70f9a1e7f6bc
GraydonvG <graydonvg@gmail.com>
2023-03-31 14:56:39 +0200

    adding loading spinner component to shop component

--- src/routes/shop/shop.route.jsx
+++ src/routes/shop/shop.route.jsx
@@ -1,29 +1,40 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { Fragment, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
-import { fetchCategoriesAsync } from '../../store/categories/categories.action';
 
 import CategoriesPreview from '../categories-preview/categories-preview.route';
 import Category from '../category/category.route';
 
+import { fetchCategoriesAsync } from '../../store/categories/categories.action';
+import { selectCategoriesIsLoading } from '../../store/categories/categories.selector';
+
+import Spinner from '../../components/spinner/spinner.component';
+
 function Shop() {
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectCategoriesIsLoading);
 
   useEffect(() => {
     dispatch(fetchCategoriesAsync());
   }, [dispatch]);
 
   return (
-    <Routes>
-      <Route
-        index
-        element={<CategoriesPreview />}
-      />
-      <Route
-        path=":category"
-        element={<Category />}
-      />
-    </Routes>
+    <Fragment>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <Routes>
+          <Route
+            index
+            element={<CategoriesPreview />}
+          />
+          <Route
+            path=":category"
+            element={<Category />}
+          />
+        </Routes>
+      )}
+    </Fragment>
   );
 }
 
