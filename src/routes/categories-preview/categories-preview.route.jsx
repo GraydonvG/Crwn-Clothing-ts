fbbaa502f0ee5eb77beccca389d9c05d06786fc8
GraydonvG <graydonvg@gmail.com>
2023-04-06 14:36:47 +0200

    adding loading spinner to categories preview

--- src/routes/categories-preview/categories-preview.route.jsx
+++ src/routes/categories-preview/categories-preview.route.jsx
@@ -1,25 +1,34 @@
+import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
 
-import { selectCategoriesMap } from '../../store/categories/categories.selector';
+import { selectCategoriesMap, selectCategoriesIsLoading } from '../../store/categories/categories.selector';
 
 import CategoryPreview from '../../components/category-preview/category-preview.component';
+import Spinner from '../../components/spinner/spinner.component';
 
 function CategoriesPreview() {
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
 
   return (
-    <div className="categories-preview-container">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview
-            key={title}
-            title={title}
-            products={products}
-          />
-        );
-      })}
-    </div>
+    <Fragment>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="categories-preview-container">
+          {Object.keys(categoriesMap).map((title) => {
+            const products = categoriesMap[title];
+            return (
+              <CategoryPreview
+                key={title}
+                title={title}
+                products={products}
+              />
+            );
+          })}
+        </div>
+      )}
+    </Fragment>
   );
 }
 
--- src/routes/shop/shop.route.jsx
+++ src/routes/shop/shop.route.jsx
@@ -1,40 +1,30 @@
-import { Fragment, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
+
+import { fetchCategoriesAsync } from '../../store/categories/categories.action';
 
 import CategoriesPreview from '../categories-preview/categories-preview.route';
 import Category from '../category/category.route';
 
-import { fetchCategoriesAsync } from '../../store/categories/categories.action';
-import { selectCategoriesIsLoading } from '../../store/categories/categories.selector';
-
-import Spinner from '../../components/spinner/spinner.component';
-
 function Shop() {
   const dispatch = useDispatch();
-  const isLoading = useSelector(selectCategoriesIsLoading);
 
   useEffect(() => {
     dispatch(fetchCategoriesAsync());
   }, [dispatch]);
 
   return (
-    <Fragment>
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        <Routes>
-          <Route
-            index
-            element={<CategoriesPreview />}
-          />
-          <Route
-            path=":category"
-            element={<Category />}
-          />
-        </Routes>
-      )}
-    </Fragment>
+    <Routes>
+      <Route
+        index
+        element={<CategoriesPreview />}
+      />
+      <Route
+        path=":category"
+        element={<Category />}
+      />
+    </Routes>
   );
 }
 
