b7bd0fb7636cb0ef6ce6c4dc2a1f1c965ca9b30e
GraydonvG <graydonvg@gmail.com>
2023-05-27 17:22:26 +0200

    converting to pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,41 @@
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA, type VitePWAOptions } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react-swc';
 import eslint from 'vite-plugin-eslint';
 import svgr from 'vite-plugin-svgr';
 
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: 'prompt',
+  includeAssets: ['favicon.icon', 'crown.svg', 'shopping-bag.svg'],
+  manifest: {
+    name: 'CRWN Clothing',
+    short_name: 'CRWN Clothing',
+    description: 'An e-commerce site for buying clothing items',
+    icons: [
+      {
+        src: '../../assets/favicon/crwn-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+      {
+        src: '../../assets/favicon/crwn-512x512.png.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+    start_url: '.',
+    display: 'standalone',
+    theme_color: '#000000',
+    background_color: '#ffffff',
+    orientation: 'portrait',
+  },
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), eslint(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [react(), svgr(), eslint(), VitePWA(manifestForPlugin)],
   build: {
     chunkSizeWarningLimit: 1600,
   },
