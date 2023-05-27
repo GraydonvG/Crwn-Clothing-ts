058bbd7b8ab940f54a1be1a24d89eb802ba2ed47
GraydonvG <graydonvg@gmail.com>
2023-05-27 16:49:00 +0200

    updating favicon and manifest.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react-swc';
 import eslint from 'vite-plugin-eslint';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), eslint()],
+  plugins: [react(), svgr(), eslint(), VitePWA({ registerType: 'autoUpdate' })],
   build: {
     chunkSizeWarningLimit: 1600,
   },
