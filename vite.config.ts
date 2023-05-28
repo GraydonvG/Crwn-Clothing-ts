a42f5f8ba16cb760762046ebf64cc193162a5766
GraydonvG <graydonvg@gmail.com>
2023-05-28 15:40:10 +0200

    improving accessibility

--- vite.config.ts
+++ vite.config.ts
@@ -31,10 +31,17 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
   },
 };
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), VitePWA(manifestForPlugin)],
   build: {
-    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
   },
 });
