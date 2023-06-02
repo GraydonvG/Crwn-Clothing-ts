58b9983a5588581bc511a2cb2c6f4dd47176aeba
GraydonvG <graydonvg@gmail.com>
2023-06-02 13:02:16 +0200

    removing lazy loading directory

--- src/components/directory/directory.component.tsx
+++ src/components/directory/directory.component.tsx
@@ -1,28 +1,23 @@
-import { lazy, Suspense } from 'react';
-
 import { categoriesData } from '../../utils/categories-data/categories-data.utility';
 
-const DirectoryItem = lazy(() => import('../directory-item/directory-item.component'));
+import DirectoryItem from '../directory-item/directory-item.component';
 
 import './directory.styles.scss';
-import Spinner from '../spinner/spinner.component';
 
 function Directory() {
   return (
-    <Suspense fallback={<Spinner />}>
-      <div className="direcory-container">
-        <div className="directory">
-          {categoriesData.map(({ id, title, imageSrc, route }) => (
-            <DirectoryItem
-              key={id}
-              title={title}
-              imageSrc={imageSrc}
-              route={route}
-            />
-          ))}
-        </div>
+    <div className="direcory-container">
+      <div className="directory">
+        {categoriesData.map(({ id, title, imageSrc, route }) => (
+          <DirectoryItem
+            key={id}
+            title={title}
+            imageSrc={imageSrc}
+            route={route}
+          />
+        ))}
       </div>
-    </Suspense>
+    </div>
   );
 }
 
